59868d46e88175ff8c495d47a0f2ba99f35a5410
LL-99 <lucas.leder99@gmail.com>
2026-04-25 15:35:59 +0200

    cache versioning fix

--- Services/ContentService.cs
+++ Services/ContentService.cs
@@ -13,24 +13,28 @@ namespace Services
 
     public class ContentService(HttpClient httpClient)
     {
+        private static readonly string ContentVersion = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+
         public async Task<List<HomePageProjectDefinition>> GetHomePageProjects()
         {
-            var json = await httpClient.GetStringAsync("data/projects-overview.json");
+            var json = await httpClient.GetStringAsync(VersionedContentUrl("data/projects-overview.json"));
 
             return JsonConvert.DeserializeObject<List<HomePageProjectDefinition>>(json) ?? [];
         }
 
         public async Task<List<DetailedProjectDefinition>> GetProjectsForGroup(ProjectGroup projectGroup)
         {
-            var json = await httpClient.GetStringAsync("data/projects-" + (projectGroup switch
+            var json = await httpClient.GetStringAsync(VersionedContentUrl("data/projects-" + (projectGroup switch
             {
                 ProjectGroup.Far => "student-assistant",
                 ProjectGroup.Uni => "university",
                 ProjectGroup.BB => "brainboost",
                 _ => "own",
-            }) + ".json");
+            }) + ".json"));
 
             return JsonConvert.DeserializeObject<List<DetailedProjectDefinition>>(json) ?? [];
         }
+
+        private static string VersionedContentUrl(string path) => $"{path}?v={ContentVersion}";
     }
 }
